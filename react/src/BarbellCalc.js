a1ddb1c14b5ed309876eb99c2b37367645a175ad
Suyog Sonwalkar <suyog@chronotale.com>
2018-08-19 18:36:09 -0400

    Refactoring BarbellCalc into its own file.

---
+++ react/src/BarbellCalc.js
@@ -0,0 +1,55 @@
+
+import React from 'react';
+
+import twentyfivelbs from './barbellcalc/25lbs.png'
+import fortyfivelbs from './barbellcalc/45lbs.png'
+import tenlbs from './barbellcalc/10lbs.png'
+import fivelbs from './barbellcalc/5lbs.png'
+import twolbs from './barbellcalc/2lbs.png'
+import onelbs from './barbellcalc/1lbs.png' 
+import collarlink from './barbellcalc/collar.png' 
+
+const plateinv = [ 
+    [45,   6],
+    [25,   2],
+    [15,   0  ],
+    [10,   2],
+    [5,    2],
+    [2.5,  2],
+    [1.25, 2],
+]
+const bar = 45; // pounds
+const collar = 5; // pounds
+
+var loadedbar = [ 
+    [45,   0,<img src={fortyfivelbs} alt='45'/>],
+    [25,   0,<img src={twentyfivelbs} alt='25'/>  ],
+    [15,0,  <img src={fortyfivelbs} alt='15' /> ],
+    [10,   0, <img src={tenlbs} alt='10'/>],
+    [5,    0, <img src={fivelbs} alt='5' />],
+    [2.5,  0, <img src={twolbs} alt='2.5'/>],
+    [1.25, 0, <img src={onelbs} alt ='1.5'/>],
+]
+const collarimg =  <img src={collarlink} alt='collar'/>
+  
+class BarbellCalc extends React.Component{
+    render() {
+        var remainder= this.props.lifters[this.props.currevent][this.props.currattempt][this.props.currlifter][1] - bar - collar*2;
+        var bar_rhs =[];
+        for (var i =0; i<loadedbar.length; i++){
+            loadedbar[i][1]=Math.min(Math.floor(remainder/(2*loadedbar[i][0])) , plateinv[i][1] );
+            remainder= remainder - loadedbar[i][0]* loadedbar[i][1] *2;
+            
+            for(var j=0; j<loadedbar[i][1];j++){
+              bar_rhs.push(loadedbar[i][2])
+            };
+        };
+        bar_rhs.push(collarimg)
+        console.log(loadedbar)
+        return(
+            <span>{bar_rhs}</span>
+        )
+    }
+};
+  
+export default BarbellCalc;
--- react/src/index.js
+++ react/src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import BarbellCalc from './BarbellCalc';
 import ReactTable from "react-table";
 import ReactCountdownClock from "react-countdown-clock";
-import twentyfivelbs from './barbellcalc/25lbs.png'
-import fortyfivelbs from './barbellcalc/45lbs.png'
-import tenlbs from './barbellcalc/10lbs.png'
-import fivelbs from './barbellcalc/5lbs.png'
-import twolbs from './barbellcalc/2lbs.png'
-import onelbs from './barbellcalc/1lbs.png' 
-import collarlink from './barbellcalc/collar.png' 
 import "react-table/react-table.css";
 
 var lifters =[ 
@@ -154,7 +148,6 @@ class App extends React.Component {
 class Liftertable extends React.Component{
 
   render() {
-
     var arrayofobj =[];
     for (var person=0; person < this.props.lifters[0][0].length; ++person){ // traverse through lifters 
       var lifterobject ={ name:     this.props.lifters[0][0][person][0],
@@ -245,50 +238,6 @@ class Liftertable extends React.Component{
     );
   }
 }
-
-const plateinv = [ 
-  [45,   6],
-  [25,   2],
-  [15,   0  ],
-  [10,   2],
-  [5,    2],
-  [2.5,  2],
-  [1.25, 2],
-]
-const bar = 45; // pounds
-const collar = 5; // pounds
-
-var loadedbar = [ 
-  [45,   0,<img src={fortyfivelbs} alt='45'/>],
-  [25,   0,<img src={twentyfivelbs} alt='25'/>  ],
-  [15,0,  <img src={fortyfivelbs} alt='15' /> ],
-  [10,   0, <img src={tenlbs} alt='10'/>],
-  [5,    0, <img src={fivelbs} alt='5' />],
-  [2.5,  0, <img src={twolbs} alt='2.5'/>],
-  [1.25, 0, <img src={onelbs} alt ='1.5'/>],
-]
-const collarimg =  <img src={collarlink} alt='collar'/>
-
-class BarbellCalc extends React.Component{
-  render() {
-      var remainder= this.props.lifters[this.props.currevent][this.props.currattempt][this.props.currlifter][1] - bar - collar*2;
-      var bar_rhs =[];
-      for (var i =0; i<loadedbar.length; i++){
-          loadedbar[i][1]=Math.min(Math.floor(remainder/(2*loadedbar[i][0])) , plateinv[i][1] );
-          remainder= remainder - loadedbar[i][0]* loadedbar[i][1] *2;
-          
-          for(var j=0; j<loadedbar[i][1];j++){
-            bar_rhs.push(loadedbar[i][2])
-          };
-      };
-      bar_rhs.push(collarimg)
-      console.log(loadedbar)
-      return(
-          <span> {bar_rhs}    </span>
-      )
-  }
-
-};
 
 ReactDOM.render(
   <App />,
